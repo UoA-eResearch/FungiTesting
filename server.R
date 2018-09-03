9199795eab73e936b5e423c91d72a24fbeb3b0fa
Bianca Haux <bhau119@UoA.auckland.ac.nz>
2018-09-03 16:13:02 +1200

    Add charts and data input.

--- server.R
+++ server.R
@@ -3,10 +3,17 @@ library(plotly)
 
 server <- function(input, output) {
   
+  ####################################################################################################
+  ############### Load data from files 
+  ####################################################################################################
   datTest <- read.csv("FungiTesting.csv")
   datMeas <- read.csv("FungiMeasurements.csv")
   data <- datTest[order(datTest$DaysOld),]
   
+  
+  ####################################################################################################
+  ############### Subset the data depending on what the user selects (Reactive data)
+  ####################################################################################################
   reactiveL <- reactive({
     if(input$L){ ret <- "L" }
     else{ret <- ""}
@@ -27,6 +34,9 @@ server <- function(input, output) {
     else{ ret <- ""}
   })
   
+  ####################################################################################################
+  ############### Reactive data from FungiTesting.csv
+  ####################################################################################################
   reactiveDataChosen <- reactive({
     dataL <- data[(data$Condition == as.character(reactiveL())), ]
     dataD <- data[(data$Condition == as.character(reactiveD())), ]
@@ -43,12 +53,33 @@ server <- function(input, output) {
     
     })
   
+  ####################################################################################################
+  ############### Reactive data from FungiMeasurements.csv
+  ####################################################################################################
+  reactiveDataChosenMeas <- reactive({
+    dataL <- datMeas[(datMeas$Condition == as.character(reactiveL())), ]
+    dataD <- datMeas[(datMeas$Condition == as.character(reactiveD())), ]
+    
+    if(!input$Media == "All"){
+      dataM <- datMeas[(datMeas$Media == as.character(input$Media)), ]
+    }
+    else{dataM <- datMeas}
+    
+    datMeasure <- union(dataL, dataD)
+    
+    datMeasure <- intersect(datMeasure, dataM)
+    
+  })
   
+  ####################################################################################################
+  ############### Plot 1 Scatter Chart - Correlation of Fungi Size and Age
+  ####################################################################################################
   output$plot1 <- renderPlotly({
     dataCurrent <- reactiveDataChosen()
     age <- dataCurrent$DaysOld
     size <- dataCurrent$Size
     zoisize <- dataCurrent$ZOISize
+    
     
     p <- plot_ly(
       type = 'scatter',
@@ -59,13 +90,26 @@ server <- function(input, output) {
       symbol = ~dataCurrent$TestedAgainst,
       symbols = c('circle','x','o'),
       mode = 'markers'
+      ) %>%
+      layout(
+        xaxis = list(title = "Age (Days)"),
+        yaxis = list(title = "Size (%)")
       )
   })
   
+  
+  ####################################################################################################
+  ############### Output table of data that is currently selected
+  ####################################################################################################
   output$tab <- renderTable({
       reactiveDataChosen()
-    })
+    
+  })
   
+  
+  ####################################################################################################
+  ############### Plot 2 Pie Chart - What media produces the largest zones on average
+  ####################################################################################################
   
   colors <- c('rgb(211,94,96)', 'rgb(128,133,133)', 'rgb(144,103,167)', 'rgb(171,104,87)', 'rgb(114,147,203)', 'rgb(51, 102, 0)')
   
@@ -89,15 +133,99 @@ server <- function(input, output) {
                  text = ~paste('Average Size: ', round(meansList, 2), ' mm diameter'),
                  marker = list(colors = colors,
                                line = list(color = '#FFFFFF', width = 1)),
-                 #The 'pull' attribute can also be used to create space between the sectors
+                 pull = 0.05,
                  showlegend = FALSE) %>%
-      layout(title = 'Average Zones Produced by Media',
+      layout(
              xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
              yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
   })
   
-  perc <- function(num, sum){
-    val <- (num*100)/sum
-  }
   
+  ####################################################################################################
+  ############### Plot 3 Line Chart - Growth rate of the fungi depending on media and condition selected
+  ####################################################################################################
+  
+  output$plot3 <- renderPlotly({
+    
+    pl <-plot_ly()
+    current <- reactiveDataChosenMeas()
+    
+    media <- c("PDA", "MEA", "WA", "CEA", "REA", "OEA")
+    
+    for(entry in sort(unique(current$ICMP))){
+      
+      group <- na.exclude(current[(current$ICMP == entry), ])
+      
+      for(mediaType in media){
+        listDark <- na.exclude(group[(group$Media == mediaType & group$Condition=="D"), ])
+        listLight <- na.exclude(group[(group$Media == mediaType & group$Condition=="L"), ])
+        
+        ############ Add all fungi grown in dark conditions to line chart #################
+        for(rep in sort(unique(listDark$Replicate))){
+          timeSeries <- na.exclude(listDark[(listDark$Replicate == rep), ])
+          
+          size <- timeSeries$Size
+          
+          timeSeries <- timeSeries %>%
+            mutate(Change = (size - lag(size, 1))/lag(size, 1), Percent_Change = Change*100)
+          
+          age <- c(4, 8, 16, 32, 64, 128)#do.call(data.frame,lapply(timeSeries$Age, function(x) replace(x, is.infinite(x),NA)))
+          Percent_Change <- do.call(data.frame,lapply(timeSeries$Percent_Change, function(x) replace(x, is.infinite(x), 0)))
+          Percent_Change <- do.call(data.frame,lapply(timeSeries$Percent_Change, function(x) replace(x, is.na(x), 0)))
+          
+          
+          if(ncol(Percent_Change) < 6){
+            for(i in ncol(Percent_Change):5){
+              Percent_Change[[as.character(i)]] <- 0
+            }
+          }
+
+          
+          pl <- add_lines(pl, x=~age, y=c(Percent_Change[[1]], Percent_Change[[2]], Percent_Change[[3]], Percent_Change[[4]], Percent_Change[[5]], Percent_Change[[6]]),  type="scatter", mode="markers+lines")
+        }
+        
+        ############ Add all fungi grown in light conditions to line chart #################
+        for(rep in sort(unique(listLight$Replicate))){
+          timeSeries <- na.exclude(listLight[(listLight$Replicate == rep), ])
+          
+          size <- timeSeries$Size
+          
+          timeSeries <- timeSeries %>%
+            mutate(Change = (size - lag(size, 1))/lag(size, 1), Percent_Change = Change*100)
+          
+          age <- c(4, 8, 16, 32, 64, 128)#do.call(data.frame,lapply(timeSeries$Age, function(x) replace(x, is.infinite(x),NA)))
+          Percent_Change <- do.call(data.frame,lapply(timeSeries$Percent_Change, function(x) replace(x, is.infinite(x), 0)))
+          Percent_Change <- do.call(data.frame,lapply(timeSeries$Percent_Change, function(x) replace(x, is.na(x), 0)))
+          
+          
+          if(ncol(Percent_Change) < 6){
+            for(i in ncol(Percent_Change):5){
+              Percent_Change[[as.character(i)]] <- 0
+            }
+          }
+          
+          
+          pl <- add_lines(pl, x=~age, y=c(Percent_Change[[1]], Percent_Change[[2]], Percent_Change[[3]], Percent_Change[[4]], Percent_Change[[5]], Percent_Change[[6]]),  type="scatter", mode="markers+lines")
+        }
+        
+        
+      }
+    }
+    
+    pl %>%
+      layout(
+        yaxis = list(title = "Growth Rate (%)"),
+        xaxis = list(title = "Age (Days)")
+      )
+  })
+  
+  
+  ####################################################################################################
+  ############### Calculate growth rates
+  ####################################################################################################
+  
+  calculateGrowth <- function(past, present){
+    ((present - past) / past) * 100
+  }
+
 }
--- ui.R
+++ ui.R
@@ -1,69 +1,87 @@
 library(shiny)
 library(plotly)
+library(shinythemes)
 
 
-ui <- fluidPage(
-  titlePanel("Fungi Screening for Antibiotics"),
-  
+ui <- fluidPage(theme = shinytheme("flatly"),
+                
+tags$style(HTML("
+          
+          .centered {
+            text-align: center;
+          }
+          
+")),
+                
+titlePanel("Fungi Screening for Antibiotics"),
     
-      fluidRow(
-        column(6,
-          fluidRow(
-            column(6,
-                   wellPanel(
-                     h4(tags$b("Condition")),
-                     checkboxInput("L","Light", TRUE),
-                     checkboxInput("D","Dark", TRUE)
-                   )
-            ),
-            column(6,
-                   wellPanel(
-                     h4(tags$b("Tested Against")),
-                     checkboxInput("KP","Klebsiella Pneumoniae", TRUE),
-                     checkboxInput("EC","E. Coli", TRUE)
-                   )
-            )
-          ),
-          fluidRow(
-            column(6,
-                   wellPanel(
-                     selectInput("Media", h4(tags$b("Media")),
-                                 c(
-                                   "PDA" = "PDA",
-                                   "MEA" = "MEA",
-                                   "WA" = "WA",
-                                   "CEA" = "CEA",
-                                   "REA" = "REA",
-                                   "OEA" = "OEA",
-                                   "All" = "All"
-                                 )
-                     )
-                   )
-            ),
-            column(6,
-                   wellPanel(
-                     h4(tags$b("Show Values As")),
-                     checkboxInput("Average","Average", FALSE)
-                   )
-            )
-          )
-        ),
-        column(6,
-                 plotlyOutput("plot1")
+fluidRow(
+  column(2,
+    fluidRow(
+      column(12,
+        wellPanel(
+          h4(tags$b("Condition")),
+          checkboxInput("L","Light", TRUE),
+          checkboxInput("D","Dark", FALSE)
         )
-      ),
-    
-
-      fluidRow(
-        column(6,
-               plotlyOutput("plot2")
-        ),
-        column(6,
-          tableOutput("tab"),
-          verbatimTextOutput("value")
-        )
-
       )
+    ),
+    fluidRow(
+      column(12,
+        wellPanel(
+          h4(tags$b("Tested Against")),
+          checkboxInput("KP","Klebsiella Pneumoniae", TRUE),
+          checkboxInput("EC","E. Coli", TRUE)
+        )
+      )
+    ),
+    fluidRow(
+      column(12,
+             wellPanel(
+               selectInput("Media", h4(tags$b("Media")),
+                           c(
+                             "PDA" = "PDA",
+                             "MEA" = "MEA",
+                             "WA" = "WA",
+                             "CEA" = "CEA",
+                             "REA" = "REA",
+                             "OEA" = "OEA",
+                             "All" = "All"
+                           )
+               )
+             )
+      )
+    )
+  ),
+  column(5,
+         wellPanel(
+           h4(tags$b(tags$p(class = "centered", "Correlation of Fungi Size and Age"))),
+           plotlyOutput("plot1")
+         )
+         
+  ),
+  column(5,
+         wellPanel(
+           h4(tags$b(tags$p(class = "centered", "Growth Rate Depending on Media and Condition Selected"))),
+           plotlyOutput("plot3") 
+         )
+  )
+),
+    
 
+fluidRow(
+    column(7,
+           wellPanel(
+             h4(tags$b(tags$p(class = "centered", "Selected Data (FungiTesting.csv)"))),
+             tableOutput("tab") 
+           )
+    ),
+    column(5,
+           wellPanel(
+             h4(tags$b(tags$p(class = "centered", "Average Zones Produced by Media"))),
+             plotlyOutput("plot2") 
+           )
+    )
+)
   
 )
